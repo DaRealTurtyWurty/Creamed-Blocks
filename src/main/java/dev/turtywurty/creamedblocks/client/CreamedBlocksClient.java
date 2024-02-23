b8f0d84eecfb255a83711f114862707328e32ca9
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2024-02-23 14:59:28 +0000

    Getting rendering fully working

--- src/main/java/dev/turtywurty/creamedblocks/client/CreamedBlocksClient.java
+++ src/main/java/dev/turtywurty/creamedblocks/client/CreamedBlocksClient.java
@@ -5,15 +5,22 @@ import dev.turtywurty.creamedblocks.CreamedBlocks;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.fabricmc.loader.impl.lib.sat4j.core.Vec;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.BlockPos;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LightLayer;
+import net.minecraft.world.phys.Vec3;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,40 +53,60 @@ public class CreamedBlocksClient implements ClientModInitializer {
             });
         });
 
-        WorldRenderEvents.LAST.register((context) -> {
+        WorldRenderEvents.AFTER_ENTITIES.register((context) -> {
             ClientLevel level = context.world();
             if(level == null)
                 return;
 
             PoseStack poseStack = context.matrixStack();
             MultiBufferSource bufferSource = context.consumers();
-            for(BlockPos pos : CLIENT_CREAMED_BLOCKS) {
-                renderMagmaCream(pos, poseStack, bufferSource);
+            float partialTicks = context.tickDelta();
+
+            Minecraft minecraft = Minecraft.getInstance();
+            Player player = minecraft.player;
+            if(player == null || player.isSpectator() || !isHoldingMagmaCream(player))
+                return;
+
+            List<BlockPos> withinRange = CLIENT_CREAMED_BLOCKS.stream()
+                    .filter(pos -> player.blockPosition().distSqr(pos) <= 64)
+                    .toList();
+
+            for(BlockPos pos : withinRange) {
+                renderMagmaCream(pos, poseStack, bufferSource, partialTicks);
             }
         });
     }
 
-    public static void renderMagmaCream(BlockPos blockPos, PoseStack poseStack, MultiBufferSource bufferSource) {
-        if(!isCreamed(blockPos))
+    public static void renderMagmaCream(BlockPos blockPos, PoseStack poseStack, MultiBufferSource bufferSource, float partialTicks) {
+        Minecraft minecraft = Minecraft.getInstance();
+        Entity cameraEntity = minecraft.getCameraEntity();
+        if (cameraEntity == null)
             return;
 
-        Minecraft minecraft = Minecraft.getInstance();
-        Player player = minecraft.player;
-        if(player == null || player.isSpectator() || !isHoldingMagmaCream(player) || player.blockPosition().distSqr(blockPos) > 64)
-            return;
+        BlockPos abovePos = blockPos.above();
+
+        Level level = minecraft.level;
+        Vec3 above = abovePos.getCenter();
+        double x = above.x - Mth.lerp(partialTicks, cameraEntity.xOld, cameraEntity.getX());
+        double y = above.y - 2 - Mth.lerp(partialTicks, cameraEntity.yOld, cameraEntity.getY());
+        double z = above.z - Mth.lerp(partialTicks, cameraEntity.zOld, cameraEntity.getZ());
 
         ItemRenderer itemRenderer = minecraft.getItemRenderer();
         poseStack.pushPose();
-        poseStack.translate(blockPos.getX() + 0.5, blockPos.getY() + 1.5, blockPos.getZ() + 0.5);
+        poseStack.translate(x, y, z);
+        poseStack.mulPose(minecraft.getEntityRenderDispatcher().cameraOrientation());
         poseStack.scale(0.5F, 0.5F, 0.5F);
         itemRenderer.renderStatic(
                 Items.MAGMA_CREAM.getDefaultInstance(),
                 ItemDisplayContext.FIXED,
-                15728880,
+                LightTexture.pack(
+                        level.getBrightness(LightLayer.BLOCK, abovePos),
+                        level.getBrightness(LightLayer.SKY, abovePos)
+                ),
                 OverlayTexture.NO_OVERLAY,
                 poseStack,
                 bufferSource,
-                minecraft.level,
+                level,
                 0);
         poseStack.popPose();
     }
